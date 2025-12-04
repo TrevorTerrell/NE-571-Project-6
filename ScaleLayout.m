53b99b9cbdcafe09584eb37817a1092e5d1a1ea1
Trevor Terrell <tterrel5@vols.utk.edu>
2025-12-04 17:34:02 -0500

    Added back-convertion for burnup

--- ScaleLayout.m
+++ ScaleLayout.m
@@ -1,18 +1,25 @@
 function scaled_layout = ScaleLayout(layout, factor)
-% ScaleLayout   Splits each node into subnodes
+% ScaleLayout   Splits each node into subnodes or averages subnodes into nodes
 %
 %   layout  :   2D matrix holding the material ID for each node
 %   factor  :   number of 1D subdivisions (a factor of 2 will turn each unit node into a 2x2 area) 
 
-    [NX, NY] = size(layout);
-    NX = NX * factor;
-    NY = NY * factor;
+    [NX_og, NY_og] = size(layout);
+    NX = NX_og * factor;
+    NY = NY_og * factor;
 
     scaled_layout = zeros([NX, NY]);
-
-    for i = 1:NX
-        for j = 1:NY
-            scaled_layout(i,j) = layout(ceil(i/factor), ceil(j/factor));
+    if factor > 1
+        for i = 1:NX
+            for j = 1:NY
+                scaled_layout(i,j) = layout(ceil(i/factor), ceil(j/factor));
+            end
+        end
+    else
+        for i = 1:NX
+            for j = 1:NY
+                scaled_layout(i,j) = mean(mean(layout(((i-1)/factor + 1):i/factor, ((j-1)/factor + 1):j/factor)));
+            end
         end
     end
 end
